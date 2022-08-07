3d80305fd551e53ab489513485c0931cb9099f08
JN <jared.bozh@gmail.com>
2022-08-07 13:18:05 +0800

    add bar and minimal able functional

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+// import Toolbar from './components/Toolbar';
+// import SettingBar from './components/SettingBar';
+// import Canvas from './components/Canvas';
+
+const App = () => {
+	return (
+		<BrowserRouter	>
+			<div className='app'>
+				{/* <Routes>
+					<Route path='/:id' element={} />
+					<Toolbar />
+					<SettingBar />
+					<Canvas />
+					<Navigate replace
+						to={`f${(+new Date()).toString(16)}`}
+					/>
+				</Routes> */}
+			</div>
+		</BrowserRouter>
+
+	);
+};
+
+export default App;
