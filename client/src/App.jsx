b301741ed654c7950a61055fe93e91307b72b125
JN <jared.bozh@gmail.com>
2022-08-07 20:08:39 +0800

    Replace component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Toolbar from './components/Toolbar';
-import SettingBar from './components/SettingBar';
-import Canvas from './components/Canvas';
+import { BrowserRouter } from 'react-router-dom'
+// import Toolbar from './components/Toolbar';
+// import SettingBar from './components/SettingBar';
+// import Canvas from './components/Canvas';
 
 const App = () => {
 	return (
 		<BrowserRouter	>
 			<div className='app'>
-				{/* <Routes>
-					<Route path='/:id' element={} />
-					<Toolbar />
-					<SettingBar />
-					<Canvas />
-					<Navigate replace
-						to={`f${(+new Date()).toString(16)}`}
-					/>
-				</Routes> */}
-				<Toolbar />
-				<SettingBar />
-				<Canvas />
+
 			</div>
 		</BrowserRouter>
 
